bc02aa2de82e23daf844f72de8d1e73953ebca5a
Dylan Coleman <dylanjacob1100@gmail.com>
2026-07-05 14:56:09 +1000

    Updates

---
+++ assets/ledSavings-CIEW97iz.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-Ba6Ds0fE.js";var t=e(`battery`,[[`path`,{d:`M 22 14 L 22 10`,key:`nqc4tb`}],[`rect`,{x:`2`,y:`6`,width:`16`,height:`12`,rx:`2`,key:`13zb55`}]]),n=.36;function r(e,t,r){let i=Math.max(0,e-t)*r*5*365/1e3;return{kwh:i,cost:i*n}}export{t as n,r as t};
